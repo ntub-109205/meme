d79d9b4172239936d4618994208634cb4c7db5db
kevinfflxo <kevin0507a@gmail.com>
2020-05-12 01:16:57 +0800

    test insert tags sql command

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('remember_token')->nullable();
             $table->json('saved');
             $table->timestamps();
         });
