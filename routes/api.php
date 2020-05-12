db72ba2e1294c421688ccc9af337adaf9744537f
kevinfflxo <kevin0507a@gmail.com>
2020-05-12 10:05:26 +0800

    initial_api_login_1

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//待改
+Route::post('login', 'API\UserController@login');
+Route::post('register', 'API\UserController@register');
+Route::group(['middleware' => 'auth:api'], function(){
+	Route::post('details', 'API\UserController@details');
+});
