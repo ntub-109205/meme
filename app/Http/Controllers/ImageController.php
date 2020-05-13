84da4d43edffe42e5b0ff78d999fbbfc183acc86
kevinfflxo <kevin0507a@gmail.com>
2020-05-13 18:50:14 +0800

    modify template_show

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -45,7 +45,6 @@ class ImageController extends Controller
                 ON m.`template_id` = t.`id`
                 WHERE t.`category_id` = :category_id
                 AND t.`share` = 1
-                AND m.`share` = 1
                 GROUP BY m.`template_id`, `filelink`, t.`name`
                 ORDER BY COUNT(m.`template_id`) DESC
                 ", ['category_id' => $request->category_id]
